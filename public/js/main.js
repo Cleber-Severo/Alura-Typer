3aeab051be0762ba123127b20b9c10d29dc37d5f
Cléber Severo <clebersevero1998@gmail.com>
2023-01-19 22:48:39 -0300

    adding event manipulation and DOM

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,15 @@
 var frase = $(".frase").text();
 var numeroPalavras = frase.split(" ").length;
 var tamanhoFrase = $('#tamanho-frase')
-
-console.log(tamanhoFrase);
-
 tamanhoFrase.text(numeroPalavras)
+
+var campo = $('.campo-digitacao');
+campo.on("input", () => {
+    var conteudo = campo.val();
+
+    var qtdPalavras = conteudo.split(/\S+/).length - 1;
+    $('#contador-palavras').text(qtdPalavras);
+
+    var qtdCaracteres = conteudo.length;
+    $('#contador-caracteres').text(qtdCaracteres)
+});
