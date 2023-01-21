4b7f5968913347fddc81b9091d457437f2cd46d9
Cléber Severo <clebersevero1998@gmail.com>
2023-01-21 12:04:44 -0300

    Reorganazing the code by functions and calling them when the page is loaded

--- public/js/main.js
+++ public/js/main.js
@@ -1,38 +1,66 @@
 var tempoInicial = $('#tempo-digitacao').text();
 var frase = $(".frase").text();
-var numeroPalavras = frase.split(" ").length;
-var tamanhoFrase = $('#tamanho-frase')
-tamanhoFrase.text(numeroPalavras)
-
 var campo = $('.campo-digitacao');
-campo.on("input", () => {
-    var conteudo = campo.val();
 
-    var qtdPalavras = conteudo.split(/\S+/).length - 1;
-    $('#contador-palavras').text(qtdPalavras);
-
-    var qtdCaracteres = conteudo.length;
-    $('#contador-caracteres').text(qtdCaracteres)
-});
-
-var tempoRestante = $('#tempo-digitacao').text();
-campo.one('focus', () => {
-    var cronometroId =  setInterval(() => {
-        tempoRestante--;
-        $('#tempo-digitacao').text(tempoRestante);
-        console.log(tempoRestante);
-
-        if(tempoRestante < 1) {
-            campo.attr('disabled', true);
-            clearInterval(cronometroId);
-        }
-    }, 1000)
+// $ = $(document).ready()
+$(() => {
+    console.log('inicializou');
+    atualizaTamanhoFrase();
+    inicializaCronometro();
+    inicializaContadores();
+    $('#botao-reiniciar').click(reinicia)
 })
 
-$('#botao-reiniciar').click(() => {
+
+
+function inicializaContadores() {
+    
+    campo.on("input", () => {
+        
+        var conteudo = campo.val();
+        var qtdPalavras = conteudo.split(/\S+/).length - 1;
+        $('#contador-palavras').text(qtdPalavras);
+    
+        var qtdCaracteres = conteudo.length;
+        $('#contador-caracteres').text(qtdCaracteres)
+
+    });
+
+}
+
+function atualizaTamanhoFrase() {
+
+    var numeroPalavras = frase.split(" ").length;
+    var tamanhoFrase = $('#tamanho-frase')
+
+    tamanhoFrase.text(numeroPalavras)
+}
+
+function inicializaCronometro() {
+    var tempoRestante = $('#tempo-digitacao').text();
+
+    campo.one('focus', () => {
+        var cronometroId =  setInterval(() => {
+            tempoRestante--;
+            $('#tempo-digitacao').text(tempoRestante);
+            console.log(tempoRestante);
+    
+            if(tempoRestante < 1) {
+                campo.attr('disabled', true);
+                clearInterval(cronometroId);
+            }
+        }, 1000)
+    })
+
+}
+
+
+function reinicia() {
     campo.attr('disabled', false);
     campo.val('');
     $('#contador-palavras').text(0);
     $('#contador-caracteres').text(0);
     $('#tempo-digitacao').text(tempoInicial);
-})
+    inicializaCronometro();
+}
+
