024b07f91633ee3394fbf719c097c7a3c24b69ce
Cléber Severo <clebersevero1998@gmail.com>
2023-01-23 22:52:08 -0300

    creating all remove btn by creating a dom element with jquery and organizing the code by js directiories

--- public/js/main.js
+++ public/js/main.js
@@ -84,26 +84,6 @@ function inicializaMarcadores() {
 }
 
 
-function inserePlacar(){
-    var corpoTabela = $(".placar").find("tbody");
-    var usuario = "Cléber";
-    var numPalavras = $("#contador-palavras").text();
-    var botaoRemover = "<a href='#'><i class='small material-icons'>delete</i></a>";
-
-     var linha = "<tr>"+
-                    "<td>"+ usuario + "</td>"+
-                    "<td>"+ numPalavras + "</td>"+
-                    "<td>"+ botaoRemover + "</td>"+
-                "</tr>";
-
-    corpoTabela.prepend(linha);
-
-}
-
-$('.botao-remover').click(function(e) {
-    e.preventDefault();
-    $(this).parent().parent().remove();
-})
 
 function reiniciaJogo() {
     campo.attr('disabled', false);
